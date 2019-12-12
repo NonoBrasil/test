d9197b492098b9fcfa314864f818810d4a8b1f3a
Noémie Pasquier <44292207+NonoBrasil@users.noreply.github.com>
2019-12-12 22:24:26 +0000

    Add files via upload

--- Assignement_2/src/Temperature.java
+++ Assignement_2/src/Temperature.java
@@ -1,42 +1,79 @@
 
-import java.io.*;
+//import java.awt.Color;
+//import java.io.*;
 
-public class Temperature {
+//import java.awt.Canvas;
+import java.awt.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+@SuppressWarnings("serial")
 
-		        
-		        FileReader filereader = null;
-		        BufferedReader bufferedreader = null;
-		        try {
-		            filereader = new FileReader("/sys/class/thermal/thermal_zone0/temp");
-		            bufferedreader = new BufferedReader(filereader);
-		            String strCurrentLine;
-		            while ((strCurrentLine = bufferedreader.readLine()) != null) {
-		              System.out.println(strCurrentLine);
-		            }
-		          } 
-		        catch (IOException e) 
-		        {
-		            e.printStackTrace();
-		        } 
-		        finally {
-		            try {
-		              if (bufferedreader != null)
-		                bufferedreader.close();
-		              if (filereader != null)
-		                filereader.close();
-		            } 
-		            catch (IOException e) 
-		            {
-		              e.printStackTrace();
-		            }
-		          }
-		        
+public class Temperature extends Canvas{
 
-		        
-		        
-	}
+	 int width, height;
 
+	 
+     /*
+     public CustomCanvas(int width, int height){
+             this.setSize(width,height);
+             this.width = width;
+             this.height = height;
+             this.update();
+     }*/
+     
+     public void update() { this.repaint(); }
+	
+	
+    public static void main(String args[]) 
+    {
+
+    	int condition =0;
+    	while (condition!=1)
+    	{
+    		
+    		if(args.length==1)
+        	{
+        		Client theApp = new Client(args[0]);
+    		    theApp.getDate();
+    		}
+        	else
+        	{
+        		System.out.println("Error: you must provide the address of the server");
+        		System.out.println("Usage is:  java Client x.x.x.x  (e.g. java Client 192.168.7.2)");
+        		System.out.println("      or:  java Client hostname (e.g. java Client localhost)");
+        	}
+    	}
+    	    
+    	System.out.println("**. End of Application.");
+    }
+    
+    public void paint(Graphics g, int X, int Y)		///
+    {
+		//X->time
+    	//Y->temperature
+		Color Color1 = new Color(0,6, 255); //blue
+        g.setColor(Color1);
+        g.drawOval(X,Y, 10, 10);
+       
+        //pour avoir couleur random : (float)Math.random()
+        
+        
+        
+      ///DRAWING 
+   	 //The abscissa represent the time and the ordinate represent the temperature at this moment
+   	 /*Graphics G;
+   	 Color Color2 = new Color(255,0, 0); //red
+     G.setColor(Color2);
+   	 G.drawLine(0, Tmin, actual_time, Tmin);	//line representing the minimum temperature
+   	 G.drawLine(0, Tmax, actual_time, Tmax);	//line representing the maximum temperature
+   	 actual_time++;	//incrementing of the time
+   	 */
+   	 
+
+    }
+
+    
 }
+    
+       
+    
+   
