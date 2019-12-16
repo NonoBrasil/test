d2c0f1f5596676cc7425697da8ec61995387aabe
Noémie Pasquier <44292207+NonoBrasil@users.noreply.github.com>
2019-12-16 14:54:45 +0000

    Add files via upload

--- Project_2/src/Brasil/Draw.java
+++ Project_2/src/Brasil/Draw.java
@@ -15,14 +15,11 @@ public class Draw extends JPanel {
 		super();
 	}
 
-	public void update(Graphics g) {
-		paint(g);
-	}
-
 	@Override
 	public void paint(Graphics g)
 	// public void paint()
 	{
+		super.paint(g);
 		// Graphics g = getGraphics();
 
 		float time; // X->time
@@ -76,16 +73,8 @@ public class Draw extends JPanel {
 				g.fillOval(45+i*35, 495-Y, 10, 10);
 			}
 	}
-
-	public void paintComponent(Graphics g) {
-		// super.paintComponent(g);
-		// Graphics2D g2 = (Graphics2D) g;
-		// COORDINATE SYSTEM
-		g.drawLine(100, 200, 100, 700); // coordinate system - ordinate
-		g.drawLine(100, 200, 90, 210); // cross - ordinate
-		g.drawLine(100, 200, 110, 210); // cross - ordinate
-		g.drawLine(100, 700, 700, 700); // coordinate system - abscissa
-		g.drawLine(700, 700, 690, 690); // cross- abscissa
-		g.drawLine(700, 700, 690, 710); // cross - abscissa
+	
+	public void update(Graphics g) {
+		paint(g);
 	}
 }
